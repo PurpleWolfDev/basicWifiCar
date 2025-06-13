63ad3e8e7b2a023e46d1bca13b5952e6217d65cd
Purple Wolf Developer <164314142+PurpleWolfDev@users.noreply.github.com>
2025-06-13 18:39:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,18 @@ wss.on('connection', (ws) => {
   ws.send('Welcome to the WebSocket server!');
 
   ws.on('message', (msg) => {
-    // console.log('Received from client:', msg);
+   
     try {
       const parsed = JSON.parse(msg);
       if (parsed.direction) {
-        // console.log("YESS")
-        // Echo back the direction to this client
-        // ws.send(parsed.direction);
+       
         dirn = parsed.direction;
         speed = parsed.speed;
-        console.log("Parsed from client:", parsed);  // ADD THIS
+        console.log("Parsed from client:", parsed);  
   console.log("TURBO:", parsed.turbo);
-        // console.log(parsed)
+       
         speed = parsed.turbo?speed*1.25:speed;
-        // console.log(speed)
+        
       }
     } catch (err) {
       console.log('Invalid JSON:', msg);
@@ -37,9 +35,9 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Broadcast to ALL clients
+
 function broadcast(message) {
-  // console.log(wss.clients);
+
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
@@ -47,31 +45,9 @@ function broadcast(message) {
   });
 }
 
-// Example: send something every 5 seconds
-// setInterval(() => {
-//   if (dirn !== lastSent) {
-//     if(speed!==lastSpeed) {
-//       broadcast(JSON.stringify({dirn : dirn, speed: speed}));
-//       lastSent = dirn;
-//       speed = speed==80?speed:speed+10;
-//     }
-//     else {
-//       broadcast(JSON.stringify({dirn : dirn, speed: speed}));
-//       lastSent = dirn;
-//       speed = speed==80?speed:speed+10;
-//     }
-//   }
-//   else {
-//     speed = speed==0?speed:speed-10;
-//   }
-// }, 200);
 
 setInterval(() => {
-  // if(dirn == 'w' || dirn=='a' || dirn=='d' || dirn=='m' || dirn == 'n') {console.log("speed increased");speed = speed==80?speed:speed+10;}
-  // else if(dirn == 's' || dirn=='o' ||dirn == 'p') {console.log("speed decreased");speed = speed == -80?speed:speed-10;}
-  // else {
-  //   speed = speed==0?speed:Math.abs(speed)-10;
-  // }
+
    broadcast(JSON.stringify({dirn : dirn, speed: speed}));
    console.log(speed);
 }, 200);
